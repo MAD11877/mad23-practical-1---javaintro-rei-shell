66600e6b58b5138fecb1136ed29acce1104f4d9e
rei-shell <s10247620@connect.np.edu.sg>
2023-04-18 13:09:26 +0800

    update on question 5

--- Question5.java
+++ Question5.java
@@ -26,6 +26,28 @@ public class Question5 {
      */
 
     Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int[] arr = new int[n];
 
+    for (int i = 0; i < n; i++) {
+      arr[i] = in.nextInt();
+    }
+    int mode = arr[0];
+    int maxCount = 1;
+
+    for (int i = 0; i < n; i++) {
+      int count = 0;
+
+      for (int j = 0; j < n; j++) {
+        if (arr[j] == arr[i]) {
+          count++;
+        }
+      }
+      if (count > maxCount) {
+        mode = arr[i];
+        maxCount = count;
+      }
+    }
+    System.out.println(mode);
   }
 }
