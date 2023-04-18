a43d944991a978d5c6a182e294d3e9bea2cd545b
rei-shell <s10247620@connect.np.edu.sg>
2023-04-18 09:52:19 +0800

    update on question 3 and 4, did changes for question 1

--- Question3.java
+++ Question3.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
-public class Question3
-{
-  public static void main(String[] args)
-  {
+public class Question3 {
+  public static void main(String[] args) {
     /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
+     * Reads integer entered by the user, multiplies the integer by itself and print
+     * out the result.
      * 
      * Hint: in.nextInt() to read integer
      */
-     
+
     Scanner in = new Scanner(System.in);
-    
+    System.out.println("Please enter a number: ");
+    int number = in.nextInt();
+    System.out.println(number * number);
+
   }
 }
--- Question4.java
+++ Question4.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
 
-public class Question4
-{
-  public static void main(String[] args)
-  {
+public class Question4 {
+  public static void main(String[] args) {
     /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
+     * Prints a right angle triangle with *. The base of the triangle depends on the
+     * integer given.
+     * e.g.
+     * > 5
+     * *****
+     * ****
+     * ***
+     * **
+     * *
      * 
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
-     
+
     Scanner in = new Scanner(System.in);
-    
+    System.out.println("Number of base: ");
+    int number = in.nextInt();
+    for (int i = number; i <= number; number--) {
+      System.out.println("*");
+    }
   }
 }
